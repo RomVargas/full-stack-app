ad7415ff15b0d1ef8a105a0a8da3c73ca84a56ea
Roman <romvargas81@gmail.com>
2024-08-08 13:03:50 -0600

    testing front-end

--- NTD-Front/src/auth/services/authService.js
+++ NTD-Front/src/auth/services/authService.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 export const loginUser = async ({username, password}) => {
     try{
-        return await axios.post('http://localhost:8080/login', {
+        //return await axios.post('http://localhost:8080/login', {
+        return await axios.post('https://zvllf0kd-8080.usw3.devtunnels.ms/login', {
             username,
             password,
         })
