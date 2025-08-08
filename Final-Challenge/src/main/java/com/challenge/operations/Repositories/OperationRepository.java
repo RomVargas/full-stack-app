cd9fd146fdc66b2f028ca3eff160a7bd7473632c
Roman Vargas <romvargas81@gmail.com>
2025-08-08 13:22:28 -0600

    Delete Final-Challenge directory

--- Final-Challenge/src/main/java/com/challenge/operations/Repositories/OperationRepository.java
+++
@@ -1,8 +0,0 @@
-package com.challenge.operations.Repositories;
-
-import com.challenge.operations.Entities.Operation;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OperationRepository extends JpaRepository<Operation, Long> {
-    Operation findByType(String type);
-}
